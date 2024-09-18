4e5ead75ba0bff0f76c091cfb3c49d2f7a79d08d
imyaoxp <100553299+imyaoxp@users.noreply.github.com>
2024-09-18 16:13:57 +0800

    更新 speedtest.py

--- utils/litespeedtest/speedtest.py
+++ utils/litespeedtest/speedtest.py
@@ -10,8 +10,8 @@ def speedtest(subscription,output_range,other_config={'concurrency': 16, 'timeou
         timeout: Time period that cannot connect to the tested proxy
     """
     default_config = {
-        'subscription': subscription, 'outputRange': '800', 'speedtestMode': 'speedonly',
-        'concurrency': 16, 'timeout': 1
+        'subscription': subscription, 'outputRange': '600', 'speedtestMode': 'speedonly',
+        'concurrency': 16, 'timeout': 2
     }
     config = default_config
     config.update(other_config)
